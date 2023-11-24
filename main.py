8b06d0d1f7290c1f748d358ea7cf896578be9de9
EMIRHAN BAYAR <e2448181@ceng.metu.edu.tr>
2023-11-25 00:38:49 +0300

    json oluştur

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import time
 import torch
 torch.cuda.empty_cache()
 
+import json
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--audio-path", type=str, default="an4_diarize_test.wav")
@@ -37,6 +39,7 @@ if __name__ == "__main__":
     # initialize speech transcriber
     transcriber=WhisperSpeechTranscriber(ModelSize.LARGE_V2)
     # iterate over audio
+    json_dict = {}
     start_time = time.time()
     start_time = time.time()
     while True:
@@ -57,6 +60,7 @@ if __name__ == "__main__":
             print("Completed ", abs(global_end - (time.time() - start_time)), " seconds late !!!!!")
 
         for i, segment in enumerate(segments):
+            json_dict[global_start + segment.start] = {"speaker": speaker_labels[i], "text": text["segments"][i%len(text["segments"])]["text"]}
             print()
             print(f"\t Speaker {speaker_labels[i]}: {round(global_start + segment.start,3)} - {round(global_start + segment.end,3)}", end="\t \t")
             print(f"\t Text: ", end="")
@@ -68,5 +72,8 @@ if __name__ == "__main__":
         if i < len(segments) - 1:
             for j in range(i + 1, len(segments)):
                 print(text["segments"][j%len(text["segments"])]["text"], end="")
+
+    with open("result.json", "w") as f:
+        json.dump(json_dict, f)
 
     print()
