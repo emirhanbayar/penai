5d3695afb79d7bff324b7b32f362307cdb421922
EMIRHAN BAYAR <e2448181@ceng.metu.edu.tr>
2023-12-03 04:24:40 +0300

    deneme

--- transcription/whisper_transcriber.py
+++ transcription/whisper_transcriber.py
@@ -1,32 +1,34 @@
 import numpy as np
-import torch
+
 from transcription.model_config import ModelSize
 from transcription.model_cache import ModelCache
 
 import whisper
 
-
 class WhisperSpeechTranscriber:
     def __init__(self, model_size: ModelSize, language_code=None):
-        device = "cuda" if torch.cuda.is_available() else "cpu"
         self.language = language_code
-        self.model = self.initialize_model(model_size, device)
+        self.model = self.initialize_model(model_size)
         self.counter = 1
 
     @staticmethod
-    def initialize_model(model_size: ModelSize, device):
-        model_size = model_size.value
+    def initialize_model(model_size: ModelSize):
+        model_size=model_size.value
         model_in_cache = ModelCache.check_model_download(model_size=model_size)
         if not model_in_cache:
-            model = whisper.load_model(model_size, device=device)
+            model = whisper.load_model(model_size)
             ModelCache.add_downloaded_model(model_size=model_size, model=model)
             return model
         else:
             model = ModelCache.get_model(model_size=model_size)
         return model
 
+
     def get_transcription(self, audio: np.ndarray):
-        segments = self.model.transcribe(audio, word_timestamps=True)
+
+
+        segments = self.model.transcribe(
+            audio
+        )
         self.counter += 1
-        words = [word for segment in segments["segments"] for word in segment["words"]]
-        return segments, words
+        return segments
