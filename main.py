22d668ea4855e5076a558bf01b9ca3a9bf4e406e
EMIRHAN BAYAR <e2448181@ceng.metu.edu.tr>
2023-12-03 04:27:37 +0300

    deneme

--- main.py
+++ main.py
@@ -34,12 +34,11 @@ class Denoiser():
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--audio-path", type=str, default="emirhan.wav")
-    parser.add_argument("--chunk-size", type=int, default=64000)
+    parser.add_argument("--audio-path", type=str, default="an4_diarize_test.wav")
+    parser.add_argument("--chunk-size", type=int, default=32000)
     parser.add_argument("--feature-clustering-threshold", type=float, default=0.8)
     parser.add_argument("--from-previous-session", type=str, default=None)
-    parser.add_argument("--device", type=str, default="cuda")
-    parser.add_argument("--max-num-speakers", type=int, default=7)
+    parser.add_argument("--device", type=str, default="gpu")
 
 
     return parser.parse_args()
@@ -55,18 +54,11 @@ if __name__ == "__main__":
     # initialize diarizer
     diarizer = PyannoteDiarizer(device=args.device,
                                 feature_clustering_threshold=args.feature_clustering_threshold,
-                                from_previous_session=args.from_previous_session,
-                                max_num_speakers=args.max_num_speakers)
+                                from_previous_session=args.from_previous_session)
 
-    # initialize diarizer
-    diarizer = PyannoteDiarizer(
-        device=args.device,
-        feature_clustering_threshold=args.feature_clustering_threshold,
-        from_previous_session=args.from_previous_session,
-    )
 
     # initialize speech transcriber
-    transcriber = WhisperSpeechTranscriber(ModelSize.TINY)
+    transcriber=WhisperSpeechTranscriber(ModelSize.TINY)
     # iterate over audio
     start_time = time.time()
     start_time = time.time()
@@ -84,45 +76,28 @@ if __name__ == "__main__":
 
         speaker_labels, segments = diarizer.run(chunk)
 
-        text, words = transcriber.get_transcription(chunk[0, :])
+        text=transcriber.get_transcription(chunk[0, :])
         print(f"\n \n Chunk {audio_iterator.batch_pointer}", end="\t")
 
         # wait for the time that is left
         total_time = global_end - (time.time() - start_time)
         if total_time > 0:
-            print(
-                "Completed ", global_end - (time.time() - start_time), " seconds early"
-            )
+            print("Completed ", global_end - (time.time() - start_time), " seconds early")
             time.sleep(global_end - (time.time() - start_time))
         else:
-            print(
-                "Completed ",
-                abs(global_end - (time.time() - start_time)),
-                " seconds late !!!!!",
-            )
+            print("Completed ", abs(global_end - (time.time() - start_time)), " seconds late !!!!!")
 
         for i, segment in enumerate(segments):
             print()
-            print(
-                f"\t Speaker {speaker_labels[i]}: {round(global_start + segment.start,3)} - {round(global_start + segment.end,3)}",
-                end="\t \t",
-            )
-            print("\t Text: ", end="")
+            print(f"\t Speaker {speaker_labels[i]}: {round(global_start + segment.start,3)} - {round(global_start + segment.end,3)}", end="\t \t")
+            print(f"\t Text: ", end="")
             try:
-                speaker_text = ""
-                for word in words:
-                    if (
-                        segment.start <= word["start"]
-                        and word["end"] <= segment.end
-                        and word["word"] != "nd"
-                    ):
-                        speaker_text += word["word"] + " "
+                print(text["segments"][i%len(text["segments"])]["text"], end="")
             except:
                 print("")
         # print remaining time segments
-        try:
-            print(speaker_text,end="")
-        except:
-            pass
+        if i < len(segments) - 1:
+            for j in range(i + 1, len(segments)):
+                print(text["segments"][j%len(text["segments"])]["text"], end="")
 
     print()
